58e2da86c94e0fc043e1b1ab5a475b490898a42b
Ulysses Butler <ub4782@truman.edu>
2020-11-10 03:07:42 -0600

    implemented changes to design, addStudents handles majors/minors

--- html/database/deploy_db.php
+++ html/database/deploy_db.php
@@ -1,9 +1,12 @@
 <?php
 include 'common_db.php';
 
+echo "Connecting to DB\n";
 $pdo = connectDB();
 
 // Student table
+echo "Creating tables...\n";
+echo "  $student_tbl\n";
 $pdo->exec("CREATE TABLE $student_tbl (" .
   "id int PRIMARY KEY AUTO_INCREMENT," .
   "email varchar(64) NOT NULL UNIQUE," .
@@ -13,66 +16,99 @@ $pdo->exec("CREATE TABLE $student_tbl (" .
   "grad_month varchar(7))");
 
 // Major and minor tables
+echo "  $major_tbl\n";
 $pdo->exec("CREATE TABLE $major_tbl (" .
   "id int PRIMARY KEY AUTO_INCREMENT," .
   "major varchar(255) NOT NULL UNIQUE," .
-  "active BOOLEAN)");
-
+  "active boolean NOT NULL DEFAULT true)");
+echo "  $minor_tbl\n";
 $pdo->exec("CREATE TABLE $minor_tbl (" .
   "id int PRIMARY KEY AUTO_INCREMENT," .
   "minor varchar(255) NOT NULL UNIQUE," .
-  "active BOOLEAN)");
+  "active boolean NOT NULL DEFAULT true)");
 
 // Student's majors and minors
+echo "  $student_major_tbl\n";
 $pdo->exec("CREATE TABLE $student_major_tbl (" .
   "id int PRIMARY KEY AUTO_INCREMENT," .
   "student_id int NOT NULL," .
   "major_id int NOT NULL)");
 
+echo "  $student_minor_tbl\n";
 $pdo->exec("CREATE TABLE $student_minor_tbl (" .
   "id int PRIMARY KEY AUTO_INCREMENT," .
   "student_id int NOT NULL," .
   "minor_id int NOT NULL)");
 
 // Faculty table
-$pdo->exec("CREATE TABLE $student_tbl (" .
+echo "  $faculty_tbl\n";
+$pdo->exec("CREATE TABLE $faculty_tbl (" .
   "id int PRIMARY KEY AUTO_INCREMENT," .
   "email varchar(64) NOT NULL UNIQUE," .
   "first_name varchar(255)," .
   "last_name varchar(255))");
 
-// Override requests helper tables
+// Class Tables
+echo "  $department_tbl\n";
 $pdo->exec("CREATE TABLE $department_tbl (" .
   "id int PRIMARY KEY AUTO_INCREMENT," .
   "department varchar(4) NOT NULL UNIQUE," .
-  "active BOOLEAN)");
+  "active boolean NOT NULL DEFAULT true)");
 
+echo "  $class_tbl\n";
 $pdo->exec("CREATE TABLE $class_tbl (" .
   "id int PRIMARY KEY AUTO_INCREMENT," .
   "department_id int NOT NULL," .
   "class_num int NOT NULL," .
-  "active BOOLEAN)");
+  "title varchar(255) NOT NULL," .
+  "active boolean NOT NULL DEFAULT true)");
 
-$pdo->exec("CREATE TABLE $status_tbl (" .
+echo "  $semester_tbl\n";
+$pdo->exec("CREATE TABLE $semester_tbl (" .
   "id int PRIMARY KEY AUTO_INCREMENT," .
-  "status varchar(255) NOT NULL UNIQUE," .
-  "active BOOLEAN)");
+  "semester varchar(6) NOT NULL," .
+  "description varchar(255) NOT NULL," .
+  "active boolean NOT NULL DEFAULT true)");
+
+echo "  $section_tbl\n";
+$pdo->exec("CREATE TABLE $section_tbl (" .
+  "id int PRIMARY KEY AUTO_INCREMENT," .
+  "class_id int NOT NULL," .
+  "semester_id int NOT NULL," .
+  "section int NOT NULL," .
+  "crn varchar(4) NOT NULL," .
+  "info varchar(255)," .
+  "active boolean NOT NULL DEFAULT true)");
 
 // Override request table
+echo "  $request_tbl\n";
 $pdo->exec("CREATE TABLE $request_tbl (" .
   "id int PRIMARY KEY AUTO_INCREMENT," .
   "student_id int NOT NULL," .
   "last_modified datetime NOT NULL," .
-  "crn varchar(4)," .
-  "department_id int," .
-  "class_id int," .
-  "status_id int)");
+  "section_id int NOT NULL," .
+  "status enum('Received', 'Approved', 'Provisionally Approved', 'Denied', 'Requires Faculty Approval') NOT NULL," .
+  "type enum('Closed Class', 'Prerequisite', 'Other') NOT NULL," .
+  "explaination text)");
 
 // Notifications table
+echo "  $notification_tbl\n";
 $pdo->exec("CREATE TABLE $notification_tbl (" .
   "id int PRIMARY KEY AUTO_INCREMENT," .
   "sender_email varchar(64) NOT NULL," .
   "receiver_email varchar(64) NOT NULL," .
   "creation datetime NOT NULL," .
   "body text)");
+
+echo "done\n\n";
+echo "Creating indicies...\n";
+
+echo "  $student_tbl\n";
+$pdo->exec("CREATE INDEX email_inx ON $student_tbl (email)");
+echo "  $faculty_tbl\n";
+$pdo->exec("CREATE INDEX email_inx ON $faculty_tbl (email)");
+echo "done\n\n";
+
+echo "[DEPLOY SCRIPT COMPLETE]\n";
+
 ?>
--- html/database/students_db.php
+++ html/database/students_db.php
@@ -1,6 +1,9 @@
 <?php
 include 'common_db.php';
 
+/**
+ * These are objects to wrap the data stored in the databases
+ */
 class Student
 {
   public $email;
@@ -8,26 +11,32 @@ class Student
   public $last_name;
   public $banner_id;
   public $grad_month;
+  public $majors;
+  public $minors;
 
-  function __construct($email, $first_name, $last_name, $banner_id, $grad_month)
+  function __construct(string $email, string $first_name, string $last_name, string $banner_id,
+    string $grad_month, array $majors, array $minors)
   {
     $this->email = $email;
     $this->first_name = $first_name;
     $this->last_name = $last_name;
     $this->banner_id = $banner_id;
     $this->grad_month = $grad_month;
+    $this->majors = $majors;
+    $this->minors = $minors;
   }
 }
 
 /**
- * Add a student to the database
+ * Add a student to the database.
  */
 function addStudent(Student $student)
 {
-  global $student_tbl;
+  global $student_tbl, $major_tbl, $minor_tbl, $student_major_tbl, $student_minor_tbl;
 
   $pdo = connectDB();
 
+  // Insert basic student info
   $smt = $pdo->prepare("INSERT INTO $student_tbl (email, first_name, last_name, banner_id, grad_month) VALUES (:email, :first_name, :last_name, :banner_id, :grad_month)");
 
   $smt->bindParam(":email", $student->email, PDO::PARAM_STR);
@@ -37,13 +46,28 @@ function addStudent(Student $student)
   $smt->bindParam(":grad_month", $student->grad_month, PDO::PARAM_STR);
 
   $smt->execute();
+
+  // The two arrays need to be converted to strings because PDO doesn't like arrays
+  $majors = implode(', ', preg_filter('/^/', "'", preg_filter('/$/', "'", $student->majors)));
+  $minors = implode(', ', preg_filter('/^/', "'", preg_filter('/$/', "'", $student->minors)));
+
+  // Insert majors
+  $smt = $pdo->prepare("INSERT INTO $student_major_tbl (student_id, major_id) SELECT $student_tbl.id, $major_tbl.id FROM $student_tbl INNER JOIN $major_tbl WHERE $student_tbl.email=:email AND $major_tbl.major IN ($majors)");
+  $smt->bindParam(":email", $student->email, PDO::PARAM_STR);
+  $smt->execute();
+
+  // And minors
+  $smt = $pdo->prepare("INSERT INTO $student_minor_tbl (student_id, minor_id) SELECT $student_tbl.id, $minor_tbl.id FROM $student_tbl INNER JOIN $minor_tbl WHERE $student_tbl.email=:email AND $minor_tbl.minor IN ($minors)");
+  $smt->bindParam(":email", $student->email, PDO::PARAM_STR);
+  $smt->execute();
 }
 
 function printTbl()
 {
+  global $student_tbl;
   $pdo = connectDB();
 
-  print_r($pdo->query("SELECT * FROM test_tbl")->fetch());
+  print_r($pdo->query("SELECT * FROM $student_tbl")->fetchAll(PDO::FETCH_ASSOC));
 }
 
 ?>
