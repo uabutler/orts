17d80232c45706513873deeedfe15a0f66463628
Ulysses Butler <ub4782@truman.edu>
2020-11-10 13:58:24 -0600

    Created outline for class functions

---
+++ html/database/classes_db.php
@@ -0,0 +1,64 @@
+<?php
+include 'common_db.php';
+
+function addDepartment(string $department)
+{
+
+}
+
+function listDepartments(): array
+{
+
+}
+
+function removeDepartment($department)
+{
+
+}
+
+class Course
+{
+  
+}
+
+function addCourse(Course $course)
+{
+
+}
+
+function listCourses(): array
+{
+
+}
+
+class Semester
+{
+
+}
+
+function addSemester(Semester $semester)
+{
+
+}
+
+function listSemesters(): array
+{
+
+}
+
+class Section
+{
+
+}
+
+function addSection(Section $section)
+{
+
+}
+
+function listSections(): array
+{
+
+}
+
+?>
