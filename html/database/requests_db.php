bf5cd4727730f0aa0a652d0488daba16ea9c37e0
Micah Kuan <mmk5213@truman.edu>
2020-11-10 20:56:11 -0600

    update to requests_db.php

--- html/database/requests_db.php
+++ html/database/requests_db.php
@@ -15,6 +15,23 @@ class OverrideRequest
 function addOverrideRequest(Student $student, Section $section, string $status, string $type, string $explaination)
 {
   // TODO: Implement minimum
+  global $request_tbl;
+  
+  $pdo = connectDB();
+
+  $smt = $pdo->prepare("INSERT INTO $request_tbl (student_id, last_modified, section_id, status, type, explanation) VALUES (:student_id, :last_modified, :section_id, :status, :type, :explanation)");
+  $smt->bindParam(":student_id", $student->id, PDO::PARAM_STR); //STR?
+
+  $time = gmmktime();
+  $now = date("Y-m-d H:i:s", $time);
+
+  $smt->bindParam(":last_modified", $now, PDO::PARAM_STR  //STR I think?
+  $smt->bindParam(":section_id", $section->id, PDO::PARAM_STR); //STR?
+  $smt->bindParam(":status", $status, PDO::PARAM_STR);
+  $smt->bindParam(":type", $type, PDO::PARAM_STR);
+  $smt->bindParam("explanation", $explanation, PDO::PARAM_STR);
+
+  $smt->execute();
 
   // TODO: validate status, type
 }
