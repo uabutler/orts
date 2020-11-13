c4b6d2f3291fee1e6374aa143918c08f56dcaaba
Ulysses Butler <ub4782@truman.edu>
2020-11-13 00:38:18 -0600

    Started php script

--- script/ingest_sections.php
+++ script/ingest_sections.php
@@ -2,5 +2,21 @@
 include_once '../html/database/common_db.php';
 
 $pdo = connectToDB();
+=======
+$file = fopen("out.csv", "r");
+$pdo = connectDB();
+
+while(!feof($file))
+{
+  $line = fgets($file);
+
+  $crn = strtok($line, '\t');
+  $department = strtok('\t');
+  $course_num = strtok('\t');
+  $section = strtok('\t');
+  $title = strtok('\t');
+
+
+}
 
 ?>
