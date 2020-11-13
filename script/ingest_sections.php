c2139889d45c7da091004f034d56817f36302b4e
Ulysses Butler <ub4782@truman.edu>
2020-11-13 00:26:30 -0600

    Rewrote department class

--- script/ingest_sections.php
+++
@@ -1,6 +0,0 @@
-<?php
-include_once '../html/database/common_db.php';
-
-$pdo = connectToDB();
-
-?>
--- script/section_xlsx2csv.py
+++ script/section_xlsx2csv.py
@@ -1,6 +1,5 @@
 import sys
 import re
-
 from openpyxl import load_workbook
 
 if len(sys.argv) != 3:
@@ -11,15 +10,12 @@ output_file = sys.argv[2]
 
 wb = load_workbook(input_file)
 
-#ws = wb.get_sheet_by_name('CS')
-#print(ws)
-
 f = open(output_file, "w")
 
 for ws in wb._sheets:
     for row in ws.iter_rows(values_only=True):
-        if type(row[0]) == str:
+        if type(row[0]) == unicode or type(row[0]) == str:
             if re.match(r'\d{4} +\w{2,4} +\d{3} +\d{2} +.+', row[0]):
                 f.write(re.sub(r'\s+', '\t', row[0], 4) + '\n')
 
-f.close
+f.close()
