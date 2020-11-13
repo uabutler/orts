c3e6a357cb625a8202dab3dd2ef24acb2773c47f
Ulysses Butler <ub4782@truman.edu>
2020-11-12 22:52:48 -0600

    Created excel script

---
+++ script/ingest_sections.php
@@ -0,0 +1,6 @@
+<?php
+include_once '../html/database/common_db.php';
+
+$pdo = connectToDB();
+
+?>
---
+++ script/section_xlsx2csv.py
@@ -0,0 +1,25 @@
+import sys
+import re
+
+from openpyxl import load_workbook
+
+if len(sys.argv) != 3:
+    sys.exit("Usage: python " + sys.argv[0] + " input output")
+
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+
+wb = load_workbook(input_file)
+
+#ws = wb.get_sheet_by_name('CS')
+#print(ws)
+
+f = open(output_file, "w")
+
+for ws in wb._sheets:
+    for row in ws.iter_rows(values_only=True):
+        if type(row[0]) == str:
+            if re.match(r'\d{4} +\w{2,4} +\d{3} +\d{2} +.+', row[0]):
+                f.write(re.sub(r'\s+', '\t', row[0], 4) + '\n')
+
+f.close
