bd4fbeafcc6aa2b17c37c54608680a05f1601acd
Ulysses Butler <ub4782@truman.edu>
2020-11-13 09:58:24 -0600

    Finished initial version of ingestion script

--- script/ingest_sections.php
+++ script/ingest_sections.php
@@ -1,21 +1,35 @@
 <?php
 include_once '../html/database/common_db.php';
+include_once '../html/database/courses_db.php';
 
-$pdo = connectToDB();
+$pdo = connectDB();
 $file = fopen("out.csv", "r");
 $pdo = connectDB();
+$semester = Semester::getActive();
 
 while(!feof($file))
 {
   $line = fgets($file);
 
   $crn = strtok($line, '\t');
-  $department = strtok('\t');
+  $department_prefix = strtok('\t');
   $course_num = strtok('\t');
-  $section = strtok('\t');
+  $section_num = strtok('\t');
   $title = strtok('\t');
 
+  $course = Course::getCourse(Department::getDepartment($department_prefix), $course_num);
 
+  if(is_null($course))
+  {
+    $course = Course::buildCourse(Department::getDepartment($department_prefix), $course_num, $title);
+    $course->storeInDB();
+  }
+
+  $section = Section::buildSection($course, $semester, $section_num, $crn);
+
+  $section->storeInDB();
+
+  fclose($file);
 }
 
 ?>
