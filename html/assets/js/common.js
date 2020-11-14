91e833c62e3e4a0741ced64a9c34b3f6098bdf62
Brandon Ingli <bi3146@truman.edu>
2020-11-14 11:21:55 -0600

    Add Authentication Modal

--- html/assets/js/common.js
+++ html/assets/js/common.js
@@ -2,3 +2,25 @@ const VIEW_TEXT = "View";
 const HIDE_TEXT = "Hide";
 const EDIT_TEXT = "Edit";
 const BASE_URL = "http://localhost:3000/api/v1"
+
+function launchAuthenticationModal(){
+  $.get("authenticationModal.html", function(h){
+    $(h).appendTo('main');
+    $("#authenticationNeededSignIn").click(function(){
+      var win = window.open("login.php", "_blank");
+      if(win){
+        win.focus();
+        $.modal.close();
+      } else {
+        alert("Please allow popups for this website.");
+      }
+    });
+    $("#authenticationNeeded").modal({
+      escapeClose: false,
+      clickClose: false,
+      showClose: false,
+      fadeDuration: 250
+    });
+  });
+};
+
