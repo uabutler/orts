cc93c63ca1e13727d8d6e9095377e23b000e2f60
Ulysses Butler <ub4782@truman.edu>
2020-11-14 19:03:22 -0600

    Fixed bugs in request class

--- html/database/requests_db.php
+++ html/database/requests_db.php
@@ -9,16 +9,16 @@ class OverrideRequest
   public $section;
   public $last_modified;
   public $status;
-  public $type;
+  public $reason;
   public $explanation;
   
-  function __construct(Student $student, Section $section, string $last_modified, string $status, string $type, string $explanation)
+  function __construct(Student $student, Section $section, string $last_modified, string $status, string $reason, string $explanation)
   {
     $this->student = $student;
     $this->section = $section;
     $this->last_modified = $last_modified;
     $this->status = $status;
-    $this->type = $type;
+    $this->reason = $reason;
     $this->explanation = $explanation;
   }
   
@@ -29,7 +29,7 @@ class OverrideRequest
    $this->section = new Section($data);
    $this->last_modified = $last_modified;
    $this->status = $status;
-   $this->type = $type;
+   $this->reason = $reason;
    $this->explanation = $explanation;
   } 
   */
@@ -38,7 +38,7 @@ class OverrideRequest
   public function getSection()      { return $this->section; }
   public function getLastModified() { return $this->last_modified; }
   public function getStatus()       { return $this->status; }
-  public function getType()         { return $this->type; }
+  public function getReason()         { return $this->reason; }
   public function getExplanation()  { return $this->explanation; }
 
   public function storeInDB()
@@ -48,15 +48,15 @@ class OverrideRequest
     $pdo = connectDB();
     
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //Shows SQL errors
-    $smt = $pdo->prepare("INSERT INTO $request_tbl (student_id, last_modified, section_id, status, type, explanation) VALUES (:student_id, :last_modified, :section_id, :status, :type, :explanation)");
+    $smt = $pdo->prepare("INSERT INTO $request_tbl (student_id, last_modified, section_id, status, reason, explanation) VALUES (:student_id, :last_modified, :section_id, :status, :reason, :explanation)");
     
-    $qwe = OverrideRequest::getStudent()->id;
+    $qwe = OverrideRequest::getStudent()->getId();
     $smt->bindParam(":student_id", $qwe, PDO::PARAM_INT); //Does -> -> work?
-    $smt->bindParam(":last_modified", OverrideRequest::getLastModified(), PDO::PARAM_STR);
-    $smt->bindParam(":section_id", OverrideRequest::getSection()->id, PDO::PARAM_INT);
-    $smt->bindParam(":status", OverrideRequest::getStatus(), PDO::PARAM_STR);
-    $smt->bindParam(":type", OverrideRequest::getType(), PDO::PARAM_STR);
-    $smt->bindParam(":explanation", OverrideRequest::getExplanation(), PDO::PARAM_STR);
+    $smt->bindParam(":last_modified", $this->last_modified, PDO::PARAM_STR);
+    $smt->bindParam(":section_id", $qwe, PDO::PARAM_INT);
+    $smt->bindParam(":status", $this->status, PDO::PARAM_STR);
+    $smt->bindParam(":reason", $this->reason, PDO::PARAM_STR);
+    $smt->bindParam(":explanation", $this->explaination, PDO::PARAM_STR);
   
     $smt->execute();
   }
@@ -67,27 +67,23 @@ class OverrideRequest
     return getEnums($request_tbl, "status");
   }
 
-  public static function listOverrideTypes()
+  public static function listOverrideReasons()
   {
     global $request_tbl;
-    return getEnums($request_tbl, "type");
+    return getEnums($request_tbl, "reason");
   }
   
   /**
    * Constructs a new request locally
    */
-  public static function buildRequest(Student $student, Section $section, string $status, string $type, string $explanation) //Need?: OverrideRequest
+  public static function buildRequest(Student $student, Section $section, string $status, string $reason, string $explanation) //Need?: OverrideRequest
   {
-	$time = gmmktime();
+    $time = gmmktime();
     $now = date("Y-m-d H:i:s", $time);
-    if(in_array($status, OverrideRequest::listStatuses()) && in_array($type, OverrideRequest::listOverrideTypes()))
-    {
-      return new OverrideRequest($student, $section, $now, $status, $type, $explanation);
-	}
-	else
-	{
-	  return null; //null? error message?
-	}
+    if(in_array($status, OverrideRequest::listStatuses()) && in_array($reason, OverrideRequest::listOverrideReasons()))
+      return new OverrideRequest($student, $section, $now, $status, $reason, $explanation);
+    else
+      return null; //null? error message?
   }
   
   /**
@@ -95,22 +91,23 @@ class OverrideRequest
    */ 
   public static function getStudentRequests(Student $student) : array
   {
-	 global $request_tbl;
+    global $request_tbl;
   	$pdo = connectDB();
    
-	$smt = $pdo->prepare("SELECT * FROM $request_tbl");   
-	$smt->execute();   
+	  $smt = $pdo->prepare("SELECT * FROM $request_tbl");   
+	  $smt->execute();   
    
-   $requestsList = $smt->fetchAll();
-   $returnList = array();
+    $requestsList = $smt->fetchAll();
+    $returnList = array();
 
-  	foreach ($requestsList as $row){
-   	$section = getSectionById($row['section_id']);
-		$request = new OverrideRequest($student, $section, $row['last_modified'], $row['status'], $row['type'], $row['explanation']);
-		array_push($returnList, $request);
-   }
+  	foreach ($requestsList as $row)
+    {
+      $section = getSectionById($row['section_id']);
+      $request = new OverrideRequest($student, $section, $row['last_modified'], $row['status'], $row['reason'], $row['explanation']);
+      array_push($returnList, $request);
+    }
     
-   return $returnList;
+    return $returnList;
   }
   
   /**
@@ -121,15 +118,15 @@ class OverrideRequest
   	global $request_tbl;
   	$pdo = connectDB();
     
-   $smt = $pdo->prepare("SELECT * FROM $request_tbl WHERE id=:request_id LIMIT 1"); 
-   $smt->bindParam(":request_id", $id, PDO:PARAM_INT);  
-	$smt->execute();  
+    $smt = $pdo->prepare("SELECT * FROM $request_tbl WHERE id=:request_id LIMIT 1"); 
+    $smt->bindParam(":request_id", $id, PDO::PARAM_INT);  
+	  $smt->execute();  
 	    
-   $data = $smt->fetch(PDO::FETCH_ASSOC);
+    $data = $smt->fetch(PDO::FETCH_ASSOC);
    
-   $student = getStudentById($data['student_id']);
-   $section = getSectionById($data['section_id']);
-	return new OverrideRequest($student, $section, $row['last_modified'], $row['status'], $row['type'], $row['explanation']);
+    $student = getStudentById($data['student_id']);
+    $section = getSectionById($data['section_id']);
+	  return new OverrideRequest($student, $section, $row['last_modified'], $row['status'], $row['reason'], $row['explanation']);
   }
   
   /**
@@ -138,28 +135,29 @@ class OverrideRequest
   public static function getOverrideRequests() : OverrideRequest
   {
   	global $request_tbl;
-  	$pdo = connectDB();
+    $pdo = connectDB();
    
-	$smt = $pdo->prepare("SELECT * FROM $request_tbl");   
-	$smt->execute();   
+    $smt = $pdo->prepare("SELECT * FROM $request_tbl");   
+    $smt->execute();   
    
-   $requestsList = $smt->fetchAll();
-   $returnList = array();
-   foreach ($requestsList as $row){
-   	$student = getStudentById($row['student_id']);
-   	$section = getSectionById($row['section_id']);
-		$request = new OverrideRequest($student, $section, $row['last_modified'], $row['status'], $row['type'], $row['explanation']);
-		array_push($returnList, $request);
-   }
+    $requestsList = $smt->fetchAll();
+    $returnList = array();
+    foreach ($requestsList as $row)
+    {
+      $student = getStudentById($row['student_id']);
+      $section = getSectionById($row['section_id']);
+      $request = new OverrideRequest($student, $section, $row['last_modified'], $row['status'], $row['reason'], $row['explanation']);
+      array_push($returnList, $request);
+    }
     
-   return $returnList;
+    return $returnList;
   }
   
 }
 
 //TESTING
-$me = Student::buildStudent('mmk9999', 'Michael', 'Kuan', '123456780', '202060', 'Freshman', array(1), array(2));
+$me = Student::getStudent('mmk9999');
 $sec = Section::getSectionById(1);
-$ob = OverrideRequest::buildRequest($me, $sec, OverrideRequest::listStatuses()[1], OverrideRequest::listOverrideTypes()[0], 'testexplanation');
-$ob.OverrideRequest::storeInDB();
+$ob = OverrideRequest::buildRequest($me, $sec, OverrideRequest::listStatuses()[1], OverrideRequest::listOverrideReasons()[0], 'testexplanation');
+$ob->storeInDB();
 ?>
