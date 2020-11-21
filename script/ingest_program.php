f73c2e26b76dfd6e74dc266b7ee8b71b93d233b5
Ulysses Butler <ub4782@truman.edu>
2020-11-20 20:17:58 -0600

    Fixed program ingestion

--- script/ingest_program.php
+++ script/ingest_program.php
@@ -4,7 +4,7 @@ include_once '../html/database/students_db.php';
 
 $pdo = connectDB();
 
-$argc == 2 or die("Please specify input files: $argv[0] majors minors\n");
+$argc == 3 or die("Please specify input files: $argv[0] majors minors\n");
 
 echo "Establishing connection to database\n";
 $pdo = connectDB();
@@ -16,21 +16,21 @@ echo "Storing data\n";
 while(!feof($file))
 {
   $line = substr(fgets($file), 0, -1);
-  if(strlen($line) <= 1) exit();
+  if(strlen($line) <= 1) break;
   Student::addMajor($line);
 }
 
 echo "done\n";
 fclose($file);
 
-echo "Reading from minors file $argv[1]\n";
-$file = fopen($argv[1], "r");
+echo "Reading from minors file $argv[2]\n";
+$file = fopen($argv[2], "r");
 
 echo "Storing data\n";
 while(!feof($file))
 {
   $line = substr(fgets($file), 0, -1);
-  if(strlen($line) <= 1) exit();
+  if(strlen($line) <= 1) break;
   Student::addMinor($line);
 }
 
