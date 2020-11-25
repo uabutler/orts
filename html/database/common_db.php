69551d9c6a482865b0b5d8aa549964bed71e3cc3
Ulysses Butler <ub4782@truman.edu>
2020-11-25 06:22:08 -0600

    Added common_db back

---
+++ html/database/common_db.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * Here, we store the names of the tables as global variables
+ */
+
+// The database storing the information for students
+$student_tbl = "students";
+$major_tbl = "majors";
+$minor_tbl = "minors";
+$student_major_tbl = "student_majors";
+$student_minor_tbl = "student_minors";
+$faculty_tbl = "faculty";
+$department_tbl = "departments";
+$course_tbl = "courses";
+$semester_tbl = "semesters";
+$section_tbl = "sections";
+$request_tbl = "requests";
+$attachment_tbl = "attachments";
+$notification_tbl = "notifications";
+
+// This function returns a connection to the MySQL DB
+function connectDB(): PDO
+{
+    $dbname = "group1";
+    $dbhost = "localhost";
+    $user = "group1";
+    $passwd = "370145";
+
+    $dsn = "mysql:host=$dbhost;dbname=$dbname";
+
+    return new PDO($dsn, $user, $passwd);
+}
+
+function flattenResult(array $result): array
+{
+    $out = [];
+
+    foreach ($result as $row)
+        array_push($out, $row[0]);
+
+    return $out;
+}
+
+function getEnums(string $table, string $field, $pdo = null): array
+{
+    if (is_null($pdo))
+        $pdo = connectDB();
+
+    $smt = $pdo->prepare("SHOW COLUMNS FROM $table WHERE Field=:field");
+    $smt->bindParam(":field", $field, PDO::PARAM_STR);
+    $smt->execute();
+
+    return explode("','", substr($smt->fetch(PDO::FETCH_ASSOC)['Type'], 6, -2));
+}
