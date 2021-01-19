5b155c2a9f3b0f9b10b6ceac182fd1338ec9e3c6
uabutler <uabutler@live.com>
2021-01-19 08:24:49 -0600

    Removed debugging info from config.php

--- html/php/config.php
+++ html/php/config.php
@@ -5,11 +5,9 @@ if($config = parse_ini_file(__DIR__.'/../../conf/app.ini', true))
     define('DATABASE', $config['Database']);
     define('CAS_SERVER', $config['CAS']);
     define('SERVER', $config['Server']);
-    print_r(DATABASE);
 }
 else
 {
     http_response_code(500);
     header("Location: error500.php");
 }
-print_r(CAS_SERVER);
