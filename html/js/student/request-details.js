4f9fd0d8a017161ea8063525aeb8be30f150e153
Ulysses Butler <ulysses@uabutler.com>
2021-02-17 02:28:18 -0600

    Added labels to options

--- html/js/student/request-details.js
+++ html/js/student/request-details.js
@@ -3,7 +3,7 @@ function reasonSelect()
     let out = '<select class="select" id="reason">';
 
     for(let i = 0; i < REASONS.length; i++)
-        out += `<option value="${REASONS[i]}"><?= $reason ?></option>`;
+        out += `<option value="${REASONS[i]}">${REASONS[i]}</option>`;
 
     out += '</select>';
 
